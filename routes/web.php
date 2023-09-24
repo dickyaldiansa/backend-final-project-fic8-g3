bff7c4036cd72a1c1ced655cf61ef97bdd780a07
dickyaldiansa <dickyaldiansa@gmail.com>
2023-09-24 11:06:03 +0700

    login dan register

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('/pages/blank-page',['type_menu' => '']);
+    return view('pages.app.dashboard-siakad',['type_menu' => '']);
 });
+
+Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register');
+})->name('register');
+
+Route::get('/forgot', function () {
+    return view('pages.auth.auth-forgot-password');
+})->name('forgot');
+
+Route::get('/reset-password', function () {
+    return view('pages.auth.auth-reset-password');
+})->name('reset-password');
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
